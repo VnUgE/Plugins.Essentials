3d8b7f705591ed30c4f5dac08e7e626d4a622a68
vnugent <public@vaughnnugent.com>
2025-04-27 15:42:14 -0400

    Squashed commit of the following:
    commit 7dda1b3c85af62bb7838de66167ed90c6c8caff1
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sun Apr 27 14:35:33 2025 -0400
    
        remove email git repo from ci dependencies list
    
    commit b5c6e9998c69c07a27f6d2d8224a0eaa658e3c32
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sun Apr 27 14:34:41 2025 -0400
    
        package updates
    
    commit 3ce230edc039a861d9d33398eaaf0987c484e03c
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sat Apr 26 12:40:32 2025 -0400
    
        fix vauth project license update
    
    commit cb3beb67ffd657e729c47832c390eabd96dfe29d
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sat Apr 26 12:35:06 2025 -0400
    
        fix license file for vauth package
    
    commit 15cd835ab7d15a0a4503e9e2f2ee295fcdee4e27
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sat Apr 26 01:32:19 2025 -0400
    
        remove bad debug postbuild step
    
    commit 13f920d2537ee6f44db4c516b67287166cf36006
    Merge: 17f63bf 2264123
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sat Apr 26 01:26:10 2025 -0400
    
        Merge branch 'vauth' into develop
    
    commit 226412396691c7cd24dab6599e30876e2b33abd8
    Author: vnugent <public@vaughnnugent.com>
    Date:   Sat Apr 26 01:04:45 2025 -0400
    
        update args list api
    
    commit db5da9a1f510d3bcdfbf1d026aaee79b8921ac01
    Author: vnugent <public@vaughnnugent.com>
    Date:   Fri Apr 25 23:24:46 2025 -0400
    
        fix vauth unit testing
    
    commit 6d7bc368df7f574992bdc52cb95fe8d0ac8018c4
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Apr 24 14:18:57 2025 -0400
    
        add vauth tests to module tests
    
    commit c41a855e31bf4df96ec962d1b289d6055ea0ed95
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Apr 24 14:09:40 2025 -0400
    
        include changes to solution file to inclue vauth project
    
    commit e2c60cb0d6d627bad80c34fa0bfc6f90e214f3ad
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Apr 24 14:08:51 2025 -0400
    
        update vauth build taskfile
    
    commit 0d8040fd820bc904389ccd149ff0a389c061b5e7
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Apr 24 12:39:10 2025 -0400
    
        begin vauth integration
    
    commit 17f63bfa647550c91b28fc4b4f3eb222fed444b8
    Merge: 545b89e 53f8f2b
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Apr 17 15:45:50 2025 -0400
    
        merge branch 'master' into 'develop'

---
+++ apps/vauth/src/Program.cs
@@ -0,0 +1,249 @@
+﻿/*
+* Copyright (c) 2024 Vaughn Nugent
+* 
+* Package: PkiAuthenticator
+* File: Program.cs 
+*
+* PkiAuthenticator is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published
+* by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* PkiAuthenticator is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with PkiAuthenticator. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System;
+
+using VNLib.Utils.Logging;
+using VNLib.Utils.Memory;
+
+using static PkiAuthenticator.Statics;
+
+namespace PkiAuthenticator
+{
+    internal sealed class Program
+    {
+        public const string JWK_EXPORT_TEMPLATE = "You may copy your JWK public key\n\n{pk}\n";
+        public const string TOKEN_PRINT_TEMPLATE = "You may copy your authentication token \n\n{tk}\n";
+        public const string YUBIKEY_PIN_ENV_VAR_NAME = "YUBIKEY_PIN";
+        public const string SOFTWARE_PASSWORD_VAR_NAME = "CERT_PASSWORD";
+        public const string PEM_EXPORT_TEMPLATE = "You may copy your public key\n\n{cert}\n";
+
+        const string HELP_MESSAGE = @$"
+    vauth Copyright (c) Vaughn Nugent <vnpublic@proton.me> https://www.vaughnnugent.com/resources/software
+
+    This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to
+    redistribute it under certain conditions. See the license.txt file for more details.
+
+    Usage: vauth <flags>
+
+    A cross-platform hardware (YubiKey) or software backed authenticator for generating short lived 
+    OTPs for VNLib.Plugins.Essentials.Accounts enabled servers. This tool generates a signed Json Web 
+    Token (JWT) that can be used as a single factor authentication method for accounts that have a stored
+    public key. Currently the plugin requires JSON Web Keys (JWK) format for public keys. It requires
+    serial numbers, key-ids, and the public key itself, x509 is not used. You may use the --export
+    flag to export this public key in the required JWK format. This tool currently supports YubiKey
+    as a hardware authenticator, and PEM encoded x509 certificates as a software authenticator. You 
+    may use this tool to list your connected YubiKey devices, and their serial numbers.
+        
+    Command flags:
+        
+        none/default            Genereates a signed OTP (one time password) Json Web Token
+                                for authentication.
+
+        -e, --export    <pem>   Writes the public key to the screen as a JWK, or optionally
+                                PEM encoded by using the 'pem' keyword following -e.
+
+        --list-devices          Lists the device information of all connected YubiKey devices.
+                                (Ignores the --key flag)
+
+        -h, --help              Prints this help message.
+
+    Global flags:
+
+        -u, --user      <uid>   The user-id (or email address) to specify during for 
+                                authenticating. If not specified, uses the certificates CN
+                                subject value.
+
+        --sign                  Enables entering custom data to add to the OTP before signing.
+                                This allows applications to add an extra layer of authentication
+                                security. If you application requires signing data, you must set
+                                this flag.  
+
+        --software <cert file>  Runs the process using a software authenticator instead of 
+                                a YubiKey hardware authenticator. The cert file must be a 
+                                a valid x509 certificate with the public key. You must also
+                                set the private key file path.
+
+        --private-key   <file>  The path to the private key file, may be password protected.
+                                This flag is only required in software mode.
+
+        --password <password?>  Set this flag if your private key is password protected. 
+                                The password string (utf8 decoded) used to decrypt the PEM 
+                                private key file. WARNING! You should avoid setting your password 
+                                after this flag unless you have cli history disabled, otherwise 
+                                your password may be recovered from your shell history file. This 
+                                allows you to automate the authentication process. NOTE: consider 
+                                setting the {SOFTWARE_PASSWORD_VAR_NAME} environment variable before 
+                                starting the process instead of supplying the password as a flag.
+
+        --key        <serial>   Allows you to specify the serial number (int32) of the exact
+                                YubiKey to connect to if multiple keys are connected. (PIV must
+                                be enabled on the device)
+
+        --pin    <device pin>   Allows you to specify your device's pin as an argument. 
+                                WARNING! You should avoid using this flag unless you have cli
+                                history disabled, otherwise your pin may be recovered from your 
+                                history file. This allows you to automate the authentication 
+                                process. NOTE: consider setting the {YUBIKEY_PIN_ENV_VAR_NAME} environment 
+                                variable before starting the process instead.
+
+        --piv-slot    <slot>    The hexadecimal YubiKey PIV slot number override to use, defaults 
+                                to authentication (9a) slot.
+        
+        -s, --silent            Silences logs, only operation output is written to STDOUT. For pin-
+                                required operations, a --pin flag must be set, or set the {YUBIKEY_PIN_ENV_VAR_NAME} 
+                                env variable. If an op error occurs, an exit code is returned.
+
+        -v, --verbose           Enables verbose logging to be written to STDOUT, is overridden
+                                by silent mode, and will override -d debug mode.
+
+        -d, --debug             Enables debug logging to be written to STDOUT, is overridden by 
+                                silent mode.
+
+    Environment Variables
+        {SOFTWARE_PASSWORD_VAR_NAME}    The password used to decrypt the PEM encoded private key file in software mode
+        
+        {YUBIKEY_PIN_ENV_VAR_NAME}      Your secret pin used for protected yubikey operations
+
+        {MemoryUtil.SHARED_HEAP_GLOBAL_ZERO}    Force all unmanaged heap allocations to be zero filled
+
+        {MemoryUtil.SHARED_HEAP_FILE_PATH}      Specify a custom unmanaged heap allocator DLL file path
+        
+
+    This tool was created to quickly generate short lived One-Time-Passwords (OTP) or signed
+    authentication tokens (JWT) for authenticating aginst PKI endpoints, using your YubiKey's
+    authentication slot (0x9a). You may use this tool to automate a login process by using the 
+    -s flag and specifying a pin with --pin (not recommended!), or setting the {YUBIKEY_PIN_ENV_VAR_NAME} 
+    environment variable. 
+
+    A software, x509 certificate file backed, mode is also supported by using the --software flag.
+    The certificate file must be a PEM encoded certificate. You must also specify a PEM encoded private
+    key file using the --private-key flag. This file may be encrypted, and you must specify a --password 
+    flag. You may wait for a prompt, set the {SOFTWARE_PASSWORD_VAR_NAME} environment variable, or write it after 
+    the --password argument: '--password my_unsecure_password'.
+
+    Examples:
+        
+        OTP:
+            vauth.exe                       # default cert CN usename
+            vauth.exe -u 'name@example.com' # specify username
+            vauth.exe --key 1111111         # specify hardware key serial numer
+            vauth.exe -s > token.txt        # write token to a text file w/ silent mode
+            vauth.exe --piv-slot 9C         # specify a differnt PIV slot on the yubikey (in hex)
+            
+            #software mode
+            vauth.exe --software 'cert.pem' --private-key 'priv.pem'
+            vauth.exe --software 'cert.pem' --private-key 'priv.pem' --password 'mypassword'
+    
+        Export public key:
+            vauth.exe --export              # for JWK output
+            vauth.exe --export pem          # for pem encoding
+
+            #software
+            vauth.exe --software cert.pem --export pem
+
+        Sign data:
+            vauth.exe --sign                # sign data before generating OTP
+
+        List devices:
+            vauth.exe --list-devices        # only supported in hardware mode
+";
+
+        static int Main(string[] args)
+        {
+            if (CliArgs.HasArgument("-h") || CliArgs.HasArgument("--help"))
+            {
+                Console.WriteLine(HELP_MESSAGE);
+                return 0;
+            }
+
+            Log.Information("vauth © 2024 Vaughn Nugent");
+
+            int exitCode = 1;
+            try
+            {
+                //Get software or hardware authenticator
+                using IAuthenticator authenticator = CliArgs.HasArgument("--software") ? new SoftwareAuthenticator() : new HardwareAuthenticator();
+
+                //initialze the authenticator
+                if (authenticator.Initialize())
+                {
+                    //Only continue if authenticator successfully initialized
+                    if (CliArgs.HasArgument("--list-devices"))
+                    {
+                        Log.Verbose("Gathering device information");
+
+                        //List devices flag
+                        exitCode = authenticator.ListDevices();
+                    }
+                    else if (CliArgs.HasArgument("-e") || CliArgs.HasArgument("--export"))
+                    {
+                        Log.Verbose("Exporting public key");
+
+                        //Check for pem encoding flag
+                        if (CliArgs.HasArgument("pem"))
+                        {
+                            string pem = authenticator.ExportPem();
+                            Log.Information(PEM_EXPORT_TEMPLATE, pem);
+                            exitCode = 0;
+                        }
+                        else
+                        {
+                            //Print jwk
+                            string? pupKey = authenticator.ExportJwk();
+
+                            //May be null if the alg is not supported
+                            if (pupKey == null)
+                            {
+                                Log.Error("The certificate does not use a supported algorithm");
+                            }
+                            else
+                            {
+                                //Print
+                                Log.Information(JWK_EXPORT_TEMPLATE, pupKey);
+                                exitCode = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Authenticate
+                        exitCode = authenticator.GenerateOtp();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                if (Log.IsEnabled(LogLevel.Debug))
+                {
+                    Log.Error(ex);
+                }
+                else
+                {
+                    Log.Error("Operation failed. Reason: {ex}", ex.Message);
+                }
+            }
+
+            Log.Verbose("Exiting...");
+
+            return exitCode;
+        }
+    }
+}
