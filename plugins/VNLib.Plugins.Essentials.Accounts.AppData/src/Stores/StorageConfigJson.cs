dbaa6f10fe42c1a381ada416f256c177be1a261a
vnugent <public@vaughnnugent.com>
2025-08-30 13:46:08 -0400

    chore(appdata): add strongly typed storage json configuration object

---
+++ plugins/VNLib.Plugins.Essentials.Accounts.AppData/src/Stores/StorageConfigJson.cs
@@ -0,0 +1,42 @@
+﻿/*
+* Copyright (c) 2025 Vaughn Nugent
+* 
+* Library: VNLib
+* Package: VNLib.Plugins.Essentials.Accounts.AppData
+* File: StorageConfigJson.cs 
+*
+* StorageConfigJson.cs is part of VNLib.Plugins.Essentials.Accounts.AppData which 
+* is part of the larger VNLib collection of libraries and utilities.
+*
+* VNLib.Plugins.Essentials.Accounts is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Affero General Public License as 
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* VNLib.Plugins.Essentials.Accounts is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+
+using System.Text.Json.Serialization;
+
+using VNLib.Plugins.Extensions.Loading;
+using VNLib.Plugins.Extensions.Loading.Configuration;
+
+namespace VNLib.Plugins.Essentials.Accounts.AppData.Stores
+{
+    internal class StorageConfigJson : IOnConfigValidation
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = "sql";
+
+        public virtual void OnValidate()
+        {
+            Validate.NotNull(Type, "Store 'type' is required");
+        }
+    }
+}
